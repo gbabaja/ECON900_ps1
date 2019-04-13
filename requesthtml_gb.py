ae4b9d28a4188a6ef4b57c60e6b895b373009254
Guncha Babajanova <46797476+gbabaja@users.noreply.github.com>
2019-04-13 16:41:10 -0400

    final codes to request web html

--- requesthtml_gb.py
+++ requesthtml_gb.py
@@ -8,10 +8,10 @@ if not os.path.exists("html_files"):
 	os.mkdir("html_files")
 
 
-cdriver = "/Users/guncha/Documents/machine_learning/GB_HW1/chromedriver"
-driver = webdriver.Chrome(cdriver)
+cdriver = "/Users/guncha/Documents/machine_learning/ECON900_ps1/chromedriver" #must include the proper path to the driver
+driver = webdriver.Chrome(cdriver) #need driver for selenium to work.  Default is mozilla firefox
 
-for i in range(1063):
+for i in range(1062):
 	driver.get("https://boardgamegeek.com/browse/boardgame/page/" +str(i+1)) #launching the URL
 	time.sleep(50) #give the page time to load (otherwise prices do not load properly)
 	page = driver.page_source #get the html
